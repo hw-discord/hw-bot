da29f6ac6c98bdfc3a2d24af908a9a2f346c767b
BuildTools <mazlum@tuta.io>
2020-03-28 15:33:24 +0300

    Data file ve değişkenlerin güncellenmesi.

--- environments/environments.js
+++ environments/environments.js
@@ -4,4 +4,11 @@ module.exports = {
     port: 25575,
     password: 'changeme',
   },
+  discord: {
+    // eslint-disable-next-line max-len
+    token: 'token-placeholder',
+  },
+  channel: {
+    signupID: '477970824293253120',
+  },
 };
