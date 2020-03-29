2a8a223c0f9b63a6190a8c5eaa78c01607fa5475
BuildTools <mazlum@tuta.io>
2020-03-29 18:19:20 +0300

    Server ayrımı yapabilmek için değişken eklendi.

--- environments/environments.js
+++ environments/environments.js
@@ -5,8 +5,9 @@ module.exports = {
     password: 'changeme',
   },
   discord: {
+    guildID: '589415209580625930',
     // eslint-disable-next-line max-len
-    token: 'NjkzMzY5NjA5MjYwMjM2ODEw.Xn8E9w.HOkylEnTNW5PHJo6Tej9OwQq2d0',
+    token: 'token-placeholder',
   },
   channel: {
     signupID: '477970824293253120',
